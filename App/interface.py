5548ccea814458aecd7b7b2db97338d9305fbfc3
ErickEduSchmitz <75916993+ErickEduSchmitz@users.noreply.github.com>
2022-12-28 23:27:14 -0300

    Update interface.py

--- App/interface.py
+++ App/interface.py
@@ -1,12 +1,30 @@
 import pygame
 import sys
+import re
+from screeninfo import get_monitors
 
-LENGTH = 887.9
-WIDTH = 499.2
+resolucao = ''
+
+for m in get_monitors():
+    resolucao = str(m)
+    
+resolucao_altura = re.findall('width=.+?,', resolucao)
+resolucao_altura = resolucao_altura[0]
+resolucao_altura = str(resolucao_altura)
+resolucao_altura = resolucao_altura.replace('width=', '')
+resolucao_altura = resolucao_altura.replace(',', '')
+resolucao_altura = float(resolucao_altura)*0.65
+
+resolucao_largura = re.findall('height=.+?,', resolucao)
+resolucao_largura = resolucao_largura[0]
+resolucao_largura = str(resolucao_largura)
+resolucao_largura = resolucao_largura.replace('height=', '')
+resolucao_largura = resolucao_largura.replace(',', '')
+resolucao_largura = float(resolucao_largura)*0.65
 
 #Inicialization
 pygame.init()
-display = pygame.display.set_mode((LENGTH,WIDTH))
+display = pygame.display.set_mode((resolucao_altura,resolucao_largura))
 pygame.display.set_caption("Cursed Ratinho")
 
 #Main loop
